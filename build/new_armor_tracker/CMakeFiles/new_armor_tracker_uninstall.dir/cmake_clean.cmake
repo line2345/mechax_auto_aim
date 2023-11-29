02adc4eb236d8772a1f74d8d3a1036c3bfcef789
line2345 <2361398121@qq.com>
2023-11-30 01:47:08 +0800

    update0.0

--- build/new_armor_tracker/CMakeFiles/new_armor_tracker_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/new_armor_tracker_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/new_armor_tracker_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
